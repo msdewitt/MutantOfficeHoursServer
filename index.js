f7ceecf2d1b36326626fe50848c0c246bf711efe
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-21 11:17:24 -0400

    Add .env file

--- index.js
+++ index.js
@@ -2,13 +2,18 @@
 var express = require('express');
 var http = require('http');
 var firebase = require('firebase');
+var dotenv = require('dotenv');
 
 //Express server setup
 var app = express();
 var server = http.createServer(app);
+dotenv.load();
 
 //Authenticate Firebase
-
+firebase.initializeApp({
+  serviceAccount: "firebaseCredentials.json",
+  databaseURL: "https://mutant-officehours.firebaseio.com"
+});
 
 server.listen(3030, function(){
   console.log('listening on http://localhost:3030');
