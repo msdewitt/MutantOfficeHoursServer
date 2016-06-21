1e4b3ddd1a06a1bb4feb4688f8b500333e782098
Matheu DeWitt <msdewitt@bsu.edu>
2016-06-21 11:01:41 -0400

    Add private key for firebase credentials.

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
+//Create references for libraries
 var express = require('express');
 var http = require('http');
+var firebase = require('firebase');
 
+//Express server setup
 var app = express();
 var server = http.createServer(app);
+
+//Authenticate Firebase
+
 
 server.listen(3030, function(){
   console.log('listening on http://localhost:3030');
